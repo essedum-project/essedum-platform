a9a155d13f0ddfdbce0768018ba046ba5b527ab0
Shaik Subhani <shaik.subhani03+infosys@infosys.com>
2025-05-30 20:16:30 +0530

    Deleted assembly and gradio folders
    Deleted assembly and gradio folders

--- gradio/gradio-apps/start-apps2.sh
+++
@@ -1,6 +0,0 @@
-mkdir videos
-python /gradio-apps/AIFirst/VideoPlayer.py "Vehicle Damage Detect.mp4" &
-sleep 60
-python /gradio-apps/AIFirst/VideoPlayer.py "InfosysMedicalWritingPlatform_DemoVideo.mp4" &
-sleep 60
-python /gradio-apps/AIFirst/VideoPlayer.py "Using GenAI-RequirementsToCode.mp4" &
