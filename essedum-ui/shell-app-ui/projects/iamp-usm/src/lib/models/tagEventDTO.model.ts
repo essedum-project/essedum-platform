e61fb5ee17fbd90733a30a9d7d4217683f4c0024
Mamindla Yadav <mamindla.yadav@infosys.com>
2025-08-18 16:58:21 +0530

    fixed as per copilot suggestions

--- essedum-ui/shell-app-ui/projects/iamp-usm/src/lib/models/tagEventDTO.model.ts
+++ essedum-ui/shell-app-ui/projects/iamp-usm/src/lib/models/tagEventDTO.model.ts
@@ -12,7 +12,7 @@ export class TagEventDTO {
   // Portfolio specific fields
   portfolioName: string[] = [];
   portfolioDescription: string[] = [];
-  
+
   constructor(
     selectedTagList,
     selectedAdapterType,
@@ -34,87 +34,105 @@ export class TagEventDTO {
     this.selectedMlAdapterCategoryType = selectedMlAdapterCategoryType;
     this.selectedMlAdapterSpecType = selectedMlAdapterSpecType;
     this.selectedMlSpecTemplateCapabilityType =
-      selectedMlSpecTemplateCapabilityType;    this.selectedMlInstanceAdapterType = selectedMlInstanceAdapterType;
+      selectedMlSpecTemplateCapabilityType;
+    this.selectedMlInstanceAdapterType = selectedMlInstanceAdapterType;
     this.selectedMlInstanceConnectionType = selectedMlInstanceConnectionType;
-    this.selectedDatasetTopicType = selectedDatasetTopicType;
-    this.portfolioName = portfolioName;
+    this.selectedDatasetTopicType = selectedDatasetTopicType;    this.portfolioName = portfolioName;
     this.portfolioDescription = portfolioDescription;
   }
 
   getSelectedTagList() {
     return this.selectedTagList;
   }
+  
   getSelectedAdapterType() {
     return this.selectedAdapterType;
   }
+  
   getSelectedAdapterInstance() {
     return this.selectedAdapterInstance;
   }
+  
   getSelectedMlAdapterConnectionType() {
     return this.selectedMlAdapterConnectionType;
   }
+  
   getSelectedMlAdapterCategoryType() {
     return this.selectedMlAdapterCategoryType;
   }
+  
   getSelectedMlAdapterSpecType() {
     return this.selectedMlAdapterSpecType;
   }
+  
   getSelectedMlSpecTemplateCapabilityType() {
     return this.selectedMlSpecTemplateCapabilityType;
   }
+  
   getSelectedMlInstanceAdapterType() {
     return this.selectedMlInstanceAdapterType;
   }
+  
   getSelectedMlInstanceConnectionType() {
     return this.selectedMlInstanceConnectionType;
-  }  getSelectedDatasetTopicType() {
+  }
+    getSelectedDatasetTopicType() {
     return this.selectedDatasetTopicType;
   }
-  
+
   getPortfolioName() {
     return this.portfolioName;
   }
-  
+
   getPortfolioDescription() {
     return this.portfolioDescription;
   }
+  
   setSelectedTagList(selectedTagList) {
     this.selectedTagList = selectedTagList;
   }
+  
   setSelectedAdapterType(selectedAdapterType) {
     this.selectedAdapterType = selectedAdapterType;
   }
+  
   setSelectedAdapterInstance(selectedAdapterInstance) {
     this.selectedAdapterInstance = selectedAdapterInstance;
   }
+  
   setSelectedMlAdapterConnectionType(selectedMlAdapterConnectionType) {
     this.selectedMlAdapterConnectionType = selectedMlAdapterConnectionType;
   }
+  
   setSelectedMlAdapterCategoryType(selectedMlAdapterCategoryType) {
     this.selectedMlAdapterCategoryType = selectedMlAdapterCategoryType;
   }
+  
   setSelectedMlAdapterSpecType(selectedMlAdapterSpecType) {
     this.selectedMlAdapterSpecType = selectedMlAdapterSpecType;
   }
-  setSelectedMlSpecTemplateCapabilityType(
+    setSelectedMlSpecTemplateCapabilityType(
     selectedMlSpecTemplateCapabilityType
   ) {
     this.selectedMlSpecTemplateCapabilityType =
       selectedMlSpecTemplateCapabilityType;
   }
-  setSelectedMlInstanceAdapterType(selectedMlInstanceAdapterType) {
+    setSelectedMlInstanceAdapterType(selectedMlInstanceAdapterType) {
     this.selectedMlInstanceAdapterType = selectedMlInstanceAdapterType;
   }
+  
   setSelectedMlInstanceConnectionType(selectedMlInstanceConnectionType) {
     this.selectedMlInstanceConnectionType = selectedMlInstanceConnectionType;
-  }  setSelectedDatasetTopicType(selectedDatasetTopicType) {
-    this.selectedDatasetTopicType = selectedDatasetTopicType;
   }
   
+  setSelectedDatasetTopicType(selectedDatasetTopicType) {
+    this.selectedDatasetTopicType = selectedDatasetTopicType;
+  }
+
   setPortfolioName(portfolioName) {
     this.portfolioName = portfolioName;
   }
-  
+
   setPortfolioDescription(portfolioDescription) {
     this.portfolioDescription = portfolioDescription;
   }
