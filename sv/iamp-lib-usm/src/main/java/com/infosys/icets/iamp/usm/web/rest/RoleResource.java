d40a460c00cbc8bfc41e45888714ff009b2e0556
Anushka Mishra <anushka.mishra@ad.infosys.com>
2025-08-18 17:26:42 +0530

    Role management - api fix

--- sv/iamp-lib-usm/src/main/java/com/infosys/icets/iamp/usm/web/rest/RoleResource.java
+++ sv/iamp-lib-usm/src/main/java/com/infosys/icets/iamp/usm/web/rest/RoleResource.java
@@ -259,7 +259,7 @@ public class RoleResource {
 	 */
 	@GetMapping("/roles/{id}")
 	@Timed
-	public ResponseEntity<?> getRole(@PathVariable Integer id) {
+	public ResponseEntity<?> getRole(@PathVariable("id") Integer id) {
 		try {
 			log.info("getRole : Request to get Role ID: {}", id);
 			Role role = roleService.findOne(id);
@@ -287,7 +287,7 @@ public class RoleResource {
 	 */
 	@DeleteMapping("/roles/{id}")
 	@Timed
-	public ResponseEntity<?> deleteRole(@PathVariable Integer id) {
+	public ResponseEntity<?> deleteRole(@PathVariable("id") Integer id) {
 		try {
 			log.info("deleteRole : Request to delete Role by ID: {} ", id);
 			roleService.delete(roleService.findOne(id));
