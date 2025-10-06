00f5884f6e7639a8213f4a4d5f5ef0560e092c34
Rishabh Mall <rishabh.mall@infosys.com>
2025-10-06 15:52:21 +0530

    Removed old extension file

--- vs-extension/src/extension.ts
+++ vs-extension/src/extension.ts
@@ -3,7 +3,6 @@ import * as vscode from 'vscode';
 import { PipelineCardsProvider } from './app/pipeline/pipeline-cards';
 import { KeycloakAuthService, KeycloakConfig } from './auth/keycloak-auth';
 import { EssedumFileSystemProvider } from './providers/essedum-file-provider';
-import { JobLogsViewer } from './app/pipeline/job-logs-viewer';
 import { JobLogsPanelProvider } from './app/pipeline/job-logs-panel-provider';
 import { initializeSSLBypass, setupAxiosDefaults, BASE_URL } from './core/constants/api-config';
 
