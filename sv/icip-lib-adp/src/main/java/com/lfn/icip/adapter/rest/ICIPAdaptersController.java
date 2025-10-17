023f3c2d1b2b7fabf20006b26dd42e059d14cdbc
satyam.kumar28 <satyam.kumar28@infosys.com>
2025-10-17 18:42:22 +0530

    S3 fixes with connection details in sagemaker

--- sv/icip-lib-adp/src/main/java/com/lfn/icip/adapter/rest/ICIPAdaptersController.java
+++ sv/icip-lib-adp/src/main/java/com/lfn/icip/adapter/rest/ICIPAdaptersController.java
@@ -165,7 +165,8 @@ public class ICIPAdaptersController {
 			else
 				params.put(ICIPPluginConstants.INSTANCE, ICIPPluginConstants.FALSE_STRING);
 			params.put(ICIPPluginConstants.IS_CACHED, ICIPPluginConstants.FALSE_STRING);
-			params.put(ICIPPluginConstants.ADAPTER_INSTANCE, adaptername);
+            MlAdapters mlAdapter = mlAdaptersService.getMlAdapteByNameAndOrganization(adaptername, org);
+			params.put(ICIPPluginConstants.ADAPTER_INSTANCE, mlAdapter.getConnectionid());
 			params.put(ICIPPluginConstants.PROJECT, org);
 			ICIPDataset datasetForRemote = new ICIPDataset();
 			datasetForRemote.setOrganization(org);
