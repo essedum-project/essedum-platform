e71e6896aff86f1c2b07b5a56cd103dd1aad79d1
Rishabh Mall <mallrishabh52@gmail.com>
2026-07-17 13:25:22 +0530

    codacy fix

---
+++ essedum-ui/shell/src/app/core/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: false,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [],
+  rules: {},
+};
